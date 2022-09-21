bc614ad4c30ba0bcde616802a7fcd897816f0e3c
ghriphos <luisfernando._alves@hotmail.com>
2022-09-21 09:52:30 -0300

    começando a implementar as funções de login, criação de usuario já está feita. Proximos passos: implementar mudança de senha

--- userRegister.py
+++ userRegister.py
@@ -7,9 +7,8 @@ import re
 def valid_mail_characters(emailToTest):
     allowedCharacters = "^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-z]{1,3}$"
     if re.match(allowedCharacters,emailToTest):
-      return True
+        return True
     return False
-
 
 def userRegister(email, username, passwd):
     if (valid_mail_characters(email)):
@@ -23,8 +22,23 @@ def userRegister(email, username, passwd):
 
         rowsCount = mycursor.rowcount
         if rowsCount > 0:
-            return "Caso esse email seja valido, você receberá um email em sua caixa de entrada, juntamente com um código de validação do email", validateEmail.insertEmailAndValidationCode(email)
+            return validateEmail.insertEmailAndValidationCode(email)
         else:
             return "Houve um erro interno"
     return "a lista de caracteres não coincide com as permitidas no sistema, por favor, reveja os parametros informados e tente novamente."
 
+def userLogin(email, passwd):
+    mydb = dbconnect.connect()
+    mycursor = mydb.cursor()
+    sql = "select * from users where email = %s and passwd = %s ;"
+
+    val = email, passwd
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    rowsCount = mycursor.rowcount
+    if rowsCount > 0:
+        print("login bem sucedido")
+    else:
+        print('ou tu n tem cadastro ou botou errado joia?')
--- validateEmail.py
+++ validateEmail.py
@@ -5,13 +5,12 @@ def generateValidationCode():
     validationCode = random.randint(100000, 999999)
     return validationCode
 
-
 def insertEmailAndValidationCode(email):
     code = generateValidationCode()
 
     mydb = dbconnect.connect()
     mycursor = mydb.cursor()
-    sql = "insert into validationcodes (email, validationCode) values (%s, %s)"
+    sql = "insert into validation_codes (email, validation_code) values (%s, %s)"
     val = email, code
     mycursor.execute(sql,val)
 
