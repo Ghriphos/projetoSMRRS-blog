f64bbe58f4a12bb18f84e03e67657ec1a32e6be3
ghriphos <luisfernando._alves@hotmail.com>
2022-09-26 16:28:43 -0300

    configurando caminhos de rotas GET pra fazer login do usuário

--- controller.py
+++ controller.py
@@ -1,12 +1,26 @@
-from flask import Flask, request
-import userRegister
+from flask import Flask, request, render_template
+import usersUtility
 
 app = Flask(__name__)
 
-@app.route("/",methods=['POST'])
+@app.route("/login",methods=['GET'])
+def renderLogin():
+    return render_template("login.html")
+
+@app.route("/login",methods=['POST'])
+def userLogin():
+    email = request.form['email']
+    passwd = request.form['passwd']
+
+    return usersUtility.userLogin(email,passwd)
+
+@app.route("/register",methods=['POST'])
 def emailRegister():
     email = request.form['email']
     username = request.form['username']
     passwd = request.form['passwd']
     
-    return userRegister.userRegister(email, username, passwd), userRegister.valid_mail_characters(email)
+    return usersUtility.userRegister(email, username, passwd), usersUtility.valid_mail_characters(email)
+
+
+
---
+++ usersUtility.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, render_template
+import dbconnect
+import controller
+import validateEmail
+import re
+
+def valid_mail_characters(emailToTest):
+    allowedCharacters = "^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-z]{1,3}$"
+    if re.match(allowedCharacters,emailToTest):
+        return True
+    return False
+
+def userRegister(email, username, passwd):
+    if (valid_mail_characters(email)):
+        mydb = dbconnect.connect()
+        mycursor = mydb.cursor()
+        sql = "insert into users (email, username, passwd) values (%s, %s, %s)"
+        val = email, username, passwd
+        mycursor.execute(sql,val)
+
+        mydb.commit()
+
+        rowsCount = mycursor.rowcount
+        if rowsCount > 0:
+            return validateEmail.insertEmailAndValidationCode(email)
+        else:
+            return "Houve um erro interno"
+    return "a lista de caracteres não coincide com as permitidas no sistema, por favor, reveja os parametros informados e tente novamente."
+
+def userLogin(email, passwd):
+    mydb = dbconnect.connect()
+    mycursor = mydb.cursor(buffered=True)
+    sql = "select * from users where email = %s and passwd = %s;"
+
+    val = email, passwd
+    mycursor.execute(sql,val)
+
+    mydb.commit()
+
+    rowsCount = mycursor.rowcount
+    if rowsCount > 0:
+        return render_template('index.html')
+    else:
+        print('ou tu n tem cadastro ou botou errado joia?')
