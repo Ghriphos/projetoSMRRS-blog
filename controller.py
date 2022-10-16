95be5b9b019235ac5769a853e25703fa38c65352
Itallo Gabriel <nakamidev@gmail.com>
2022-10-16 22:11:23 +0000

    Juntar arquivos routes.py com controllers.py

--- controller.py
+++ controller.py
@@ -3,6 +3,18 @@ import usersUtility, posts
 
 app = Flask(__name__, static_url_path='/static/')
 
+@app.route("/",methods=['GET'])
+def renderMain():
+    return render_template("main.jinja")
+
+@app.route("/register",methods=['GET'])
+def renderRegister():
+    return render_template("register.jinja")
+
+@app.route("/login",methods=['GET'])
+def renderLogin():
+    return render_template("login.jinja")
+
 @app.route("/login",methods=['POST'])
 def userLogin():
     email = request.form['email']
