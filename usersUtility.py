34ccde631c71c0b0e6a9bb4a4daa19e814301221
Ghriphos <luisfernando._alves@hotmail.com>
2022-10-16 21:42:27 -0300

    garantindo que não tenha duplicidade de email e usuarios cadastrados

--- usersUtility.py
+++ usersUtility.py
@@ -7,22 +7,55 @@ def valid_mail_characters(emailToTest):
         return True
     return False
 
+def verifyDuplicatedUsername(username):
+    mydb = dbconnect.connect()
+    mycursor = mydb.cursor(buffered=True)
+    sql = "select * from users where username = (%s)"
+    val = (username,)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    rowsCount = mycursor.rowcount
+
+    if rowsCount > 0:
+        return False
+    else:
+        return True
+
+def verifyDuplicatedEmail(email):
+    mydb = dbconnect.connect()
+    mycursor = mydb.cursor(buffered=True)
+    sql = "select * from users where email = (%s)"
+    val = (email,)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    rowsCount = mycursor.rowcount
+
+    if rowsCount > 0:
+        return False
+    else:
+        return True
+
+
 def userRegister(email, username, passwd):
     if (valid_mail_characters(email)):
-        mydb = dbconnect.connect()
-        mycursor = mydb.cursor()
-        sql = "insert into users (email, username, passwd) values (%s, %s, %s)"
-        val = email, username, passwd
-        mycursor.execute(sql,val)
-        mydb.commit()
+        if verifyDuplicatedEmail(email):
+            if verifyDuplicatedUsername(username):
+                mydb = dbconnect.connect()
+                mycursor = mydb.cursor()
+                sql = "insert into users (email, username, passwd) values (%s, %s, %s)"
+                val = email, username, passwd
+                mycursor.execute(sql, val)
+                mydb.commit()
 
-        rowsCount = mycursor.rowcount
-        if rowsCount > 0:
-            session['username'] = username
-            print(session['username'])
-            return render_template("main.jinja")
-        else:
-            return render_template('register.jinja', error="Houve um erro interno.")
+                rowsCount = mycursor.rowcount
+                if rowsCount > 0:
+                    session['username'] = username
+                    print(session['username'])
+                    return render_template("main.jinja")
+                else:
+                    return render_template('register.jinja', error="Houve um erro interno.")
+            return render_template('register.jinja', error="Nome de usuário já foi cadastrado.")
+        return render_template('register.jinja', error="Este email já pertence a uma conta registrada.")
     return render_template('register.jinja', error="A lista de caracteres não coincide com as permitidas no sistema, por favor, reveja os parametros informados e tente novamente.")
 
 def userLogin(email, passwd):
@@ -31,7 +64,7 @@ def userLogin(email, passwd):
         mycursor = mydb.cursor(buffered=True)
         sql = "select * from users where email = %s and passwd = %s;"
         val = email, passwd
-        mycursor.execute(sql,val)
+        mycursor.execute(sql, val)
         mydb.commit()
         myresult = mycursor.fetchall()
 
